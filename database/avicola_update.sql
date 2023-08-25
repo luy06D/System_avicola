c22763e87517b66d57902be4eb9e402fc037b7e3
JeanMateo <1359806@senati.pe>
2023-08-25 11:26:48 -0500

    spu_detalle_pagos

--- database/avicola_update.sql
+++ database/avicola_update.sql
@@ -870,10 +870,6 @@ END $$
 
 
 
-
-
-
-
 DELIMITER $$
 
 CREATE PROCEDURE spu_pagos_registrar
@@ -920,42 +916,6 @@ END $$
 
 CALL spu_listar_detallesclientes(2);
 
-DELIMITER $$
-
- 
-
-CREATE PROCEDURE spu_ventas_mostrar1()
-BEGIN
-    SELECT
-        v.idventa,
-        CONCAT(cl.nombres, ' ', cl.apellidos) AS Cliente,
-        v.deuda AS deuda_total,
-        COALESCE(SUM(p.pago), 0) AS pago_total,
-        (v.deuda - COALESCE(SUM(p.pago), 0)) AS saldo,
-        CASE
-            WHEN (v.deuda - COALESCE(SUM(p.pago), 0)) = 0 THEN 'Cancelado'
-            ELSE 'Pendiente'
-        END AS estado
-    FROM ventas v
-    LEFT JOIN pagos p ON v.idventa = p.idventa
-    INNER JOIN detalle_ventas dv ON v.iddetalle_venta = dv.iddetalle_venta
-    INNER JOIN productos pr ON dv.idproducto = pr.idproducto
-    INNER JOIN clientes c ON v.idcliente = c.idcliente
-    INNER JOIN personas cl ON c.idpersona = cl.idpersona
-    GROUP BY v.idventa
-    ORDER BY p.fechapago DESC;
-END $$
-
- 
-
-DELIMITER ;
-		
-=======
-SELECT * FROM ventas
-
-
-
-
 
 
  
@@ -990,4 +950,4 @@ BEGIN
 
 
  
->>>>>>> 13c552f74d170ad7902c226caa8947e3d725fe39
+
